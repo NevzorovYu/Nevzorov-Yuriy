7b47d75f7b26eefd3ed33f58f77348cc318cb383
Yuriy Nevzorov <nevzorovyura@gmail.com>
2021-06-02 19:47:35 +0300

    Первый коммит

---
+++ main.js
@@ -0,0 +1,13 @@
+console.log("Hello world");
+let a = 25;
+alert("Welcome");
+const name = prompt("What is your name?");
+const age = prompt("How old are you?");
+console.log("Welcome to us " + name + "!" + " You are " + age + " years old.");
+console.log("My name is Siri!");
+if (age < a) 
+console.log("I'm a little older than you");
+if (age > a) 
+console.log("I'm a little younger than you");
+if (age == a) 
+console.log("Wow! We are the same age!");
