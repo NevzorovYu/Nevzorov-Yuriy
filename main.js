ca60bf0c4702b45e8096d44984f4cd0f6bffaa17
NevzorovYu <82965296+NevzorovYu@users.noreply.github.com>
2021-06-10 14:06:08 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,38 @@
-console.log("Hello world");
-let a = 25;
-alert("Welcome");
-const name = prompt("What is your name?");
-const age = prompt("How old are you?");
-console.log("Welcome to us " + name + "!" + " You are " + age + " years old.");
-console.log("My name is Siri!");
-if (age < a) 
-console.log("I'm a little older than you");
-if (age > a) 
-console.log("I'm a little younger than you");
-if (age == a) 
-console.log("Wow! We are the same age!");
+console.log("Calculator");
+let i = 0;
+alert("Welcome to primitive calculator");
+const cycle = prompt("Input the number of cycles. Only number, please.");
+for (let i = 0; i < cycle; i++) {
+    console.log("cycle - " + i);
+    const firstoper = prompt("Input first operand. Only number, please.");
+    if (Number(firstoper)) {
+        alert("ok");
+    }
+    else {
+        alert("Only number, please.");
+    }
+    const operator = prompt("Input one of operators + - * /");
+    const secondoper = prompt("Input second operand. Only number, please.");
+    if (Number(secondoper)) {
+        alert("ok");
+    }
+    else {
+        alert("Only number, please.");
+    }
+    let num1 = Number(firstoper);
+    let num2 = Number(secondoper);
+    switch (operator) {
+        case "+": result = (num1 + num2); break;
+        case "-": result = (num1 - num2); break;
+        case "*": result = (num1 * num2); break;
+        case "/": result = (num1 / num2); break;
+        default: alert("Input only operators + - * /");
+    }
+    console.log(num1 + operator + num2 + "=" + result);
+    if (Number(result)) {
+        console.log("Good");
+    }
+    else {
+        console.log("Я предупреждал :Р");
+    }
+}
